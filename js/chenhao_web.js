d917d3eadf0e6500738d9b8e085095ae1df85224
weekdawn <571426779@qq.com>
2021-01-29 21:50:00 +0800

    js跨域bug更新

--- js/chenhao_web.js
+++ js/chenhao_web.js
@@ -95,40 +95,35 @@ $(document).ready(function () {
 
     function show_table(cu_price_rate, plastic_price_rate) {
         let type = $("#type_select_query option:selected").text();
-        //type = type.toLowerCase();
         if(type != "选择型号"){
-            $.getJSON("./json/" + type + ".json", function (data) {
-                $.each(data, function (i, item) {
-                    $.each(item, function (j, i_item) {
-                        let specs = i_item["specs"];
-                        let cu_weight = parseFloat(i_item["cu_weight"]);
-                        let plastic_weight = parseFloat(i_item["plastic_weight"]);
-                        let cu_price, plastic_price;
-                        if(cu_price_rate == 0){
-                            cu_price = parseFloat(i_item["cu_price"]);
-                        }else {
-                            cu_price = cu_weight * cu_price_rate;
-                        }
-                        if(plastic_price_rate == 0){
-                            plastic_price = parseFloat(i_item["plastic_price"]);
-                        }else {
-                            plastic_price = plastic_weight * plastic_price_rate;
-                        }
-                        let other_price = parseFloat(i_item["other_price"]);
-                        let final_cost = cu_price + plastic_price + other_price;
-                        let td_unit = "<tr><td>" +
-                            specs + "</td><td>" +
-                            cu_weight.toFixed(2) + "</td><td>" +
-                            cu_price.toFixed(2) + "</td><td>" +
-                            plastic_weight.toFixed(2) + "</td><td>" +
-                            plastic_price.toFixed(2) + "</td><td>" +
-                            other_price.toFixed(2) + "</td><td>" +
-                            final_cost.toFixed(1) + "</td></tr>";
-                        $("#datalist").append(td_unit);
-                    });
-                })
+            let select_type = costdata[type];
+            $.each(select_type, function (j, i_item) {
+                let specs = i_item["specs"];
+                let cu_weight = parseFloat(i_item["cu_weight"]);    //铜重
+                let plastic_weight = parseFloat(i_item["plastic_weight"]);  //塑料重
+                let cu_price, plastic_price;
+                if(cu_price_rate == 0){
+                    cu_price = parseFloat(i_item["cu_price"]);
+                }else {
+                    cu_price = cu_weight * cu_price_rate;
+                }
+                if(plastic_price_rate == 0){
+                    plastic_price = parseFloat(i_item["plastic_price"]);
+                }else {
+                    plastic_price = plastic_weight * plastic_price_rate;
+                }
+                let other_price = parseFloat(i_item["other_price"]);
+                let final_cost = cu_price + plastic_price + other_price;
+                let td_unit = "<tr><td>" +
+                    specs + "</td><td>" +
+                    cu_weight.toFixed(2) + "</td><td>" +
+                    cu_price.toFixed(2) + "</td><td>" +
+                    plastic_weight.toFixed(2) + "</td><td>" +
+                    plastic_price.toFixed(2) + "</td><td>" +
+                    other_price.toFixed(2) + "</td><td>" +
+                    final_cost.toFixed(1) + "</td></tr>";
+                $("#datalist").append(td_unit);
             });
         }
-
     }
 });
